c4b4f07424e71d5cd0ef461eb1a2569717aac6ef
Mateus Fabricio <fabriciomateus05@gmail.com>
2019-11-08 21:16:57 -0300

    1 - Adicionado a tela de login 2 - Adicionado menu top

--- app/src/main/java/com/example/pontoeasy/MainActivity.java
+++ app/src/main/java/com/example/pontoeasy/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.pontoeasy;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    androidx.appcompat.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Objeto Toolbar
+        toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setElevation(10.f);
+        }
+    }
+
+    //método que infla o menu no toolbar do aplicativo (res/menu/list_of_users_menu.xml)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //realiza a ação de click de um dos itens do menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String msg = "";
+
+        //verifica qual dos itens foi selecionado e atribui a string msg o titulo do item
+        switch(item.getItemId()){
+            case R.id.btnPerfil:
+                msg = "perfil";
+                break;
+            case R.id.btnHistorico:
+                msg = "historico";
+                break;
+            case R.id.btnEnviar:
+                msg = "enviar";
+                break;
+            case R.id.btnLogout:
+                msg = "logout";
+                break;
+        }
+
+        Toast.makeText(this, msg + " checked", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
